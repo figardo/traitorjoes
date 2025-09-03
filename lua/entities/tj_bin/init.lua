2e209846c3dd8eb32618f68fe596d45b77d99bb2
figardo <figardo@users.noreply.github.com>
2025-09-03 21:45:24 +0100

    temporary bin functionality

--- lua/entities/tj_bin/init.lua
+++ lua/entities/tj_bin/init.lua
@@ -3,10 +3,16 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 
 ENT.Model = Model("models/props_trainstation/trashcan_indoor001b.mdl")
+ENT.CanUseKey = true
 
 function ENT:Initialize()
 	self:SetModel(self.Model)
 
 	self:PhysicsInit(SOLID_VPHYSICS)
 	self:SetMoveType(MOVETYPE_NONE)
+end
+
+function ENT:UseOverride(ply)
+	ply:Give("weapon_ttt_carbine")
+	ply:Give("weapon_ttt_beacon")
 end
